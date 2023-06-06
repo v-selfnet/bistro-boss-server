0a570920b3197104f05772167df39f8bccd0db73
Ashraful Haque <vselfnet@gmail.com>
2023-06-07 00:22:33 +0200

    Method Delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -51,6 +51,35 @@ async function run() {
       res.send(result)
     })
 
+    // 4 to view cart items in server
+    // http://localhost:5000/carts
+    // app.get('/carts', async(req, res) => {
+    //   const result = await cartCollection.find().toArray();
+    //   res.send(result);
+    // })
+
+    // 5 to view cart items Navbar.jsx
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      // console.log(email)
+      if(!email){ 
+        res.send([])
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // 6 delete [MyCart.jsx]
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
+
+   
+
     await client.db("admin").command({ ping: 1 });
     console.log("Bistro Boss Restaurant Server is successfully connected to MongoDB!");
   } finally {
@@ -65,6 +94,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is Running on port: ${port}`)
+    console.log(`Bistro Boss Restaurant Server is Running on port: ${port}`)
 })
 
